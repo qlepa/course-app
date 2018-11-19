3401ff0a67f4fcae57d164513b6e7989bebff6be
Tomasz Klepacki <t.klepacki@gmail.com>
2018-11-19 10:43:53 +0100

    Branch check

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const buttonText = { text: 'Component are cool'};
-    const labelText = { text: 'Enter yourn name:' }
+    const labelText = { text: 'Enter name:' }
 
     return (
         <div>
-            <label className="label" htmlFor="name">
-                {labelText.text}
-            </label>
-            <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {buttonText.text}
-            </button>
+            <h1>Bongorno</h1>
         </div>
     );
 };
