247f64991f8657e2c83fdfa0dd2b3d4bba469044
Tomasz Klepacki <t.klepacki@gmail.com>
2018-11-19 13:31:15 +0100

    Add a comment section + install faker and using Semanting UI. Finished section 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-    const buttonText = { text: 'Component are cool'};
-    const labelText = { text: 'Enter name:' }
-
     return (
-        <div>
-            <h1>Bongorno</h1>
+        <div className="ui container comments">
+            <ApprovalCard>
+                Are you sure you want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam"
+                               date="Today at 4:45PM"
+                               comment="Yo Yo Yo!"
+                               image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex"
+                               date="Today at 2:00AM"
+                               comment="Bla bla bla..."
+                               image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane"
+                               date="Yesterday at 5:00PM"
+                               comment="What a great blog!"
+                               image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Tom"
+                               date="Yesterday at 6:00PM"
+                               comment="Doh!"
+                               image={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
